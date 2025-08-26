25467c4575b4b601905864866b0132b9d831999e
André Luis <moralandre666@gmail.com>
2025-08-25 21:27:00 -0300

    imparoupar final

--- atividade-funcoes.js
+++ atividade-funcoes.js
@@ -1,8 +1,22 @@
-function veriimparoupar (number)
+function veriimparoupar (number) {
 
-{
-    if (number % 2 ===0) 
-        return (par)
+    if (typeof number !== 'number' || isNaN(number)) {
+        return 'Erro: Por favor, insira um número válido.'; //faz a verificação se o número é valido
+    }
+
+    
+    if (!Number.isInteger(number)) {
+        return 'Erro: A função suporta apenas números inteiros.'; // faz a verificação se o número é inteiro
+    };
+    {
+    if (number % 2 === 0)  // verifica se o número é par
+        return 'par';
     else 
-        return (impar)
+        return 'impar'; // verifica se o número é ímpar
+    };
 }
+
+let input = prompt("Qual é o número que você deseja verificar?");
+let number = parseFloat(input); 
+
+alert(veriimparoupar(number));
